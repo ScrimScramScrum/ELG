f1ba61ee3d468ad9331a4f2bed438c5d54ab769c
eiriksandberg <eirik.sandberg@live.no>
2015-01-07 11:03:34 +0100

    Writing repo classes

--- ELG-prosjekt/src/main/java/springmvc/service/MultiChoiceService.java
+++ ELG-prosjekt/src/main/java/springmvc/service/MultiChoiceService.java
@@ -6,10 +6,25 @@
 
 package springmvc.service;
 
-/**
- *
- * @author eiriksandberg
- */
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import springmvc.domain.MultiChoice;
+import springmvc.repository.MultiChoiceRepository;
+
+
 public class MultiChoiceService {
     
+    private MultiChoiceRepository repo;
+    
+    @Autowired
+    public void setRepository(MultiChoiceRepository repo){
+        System.out.println("MultiChoiceService.mock" + repo);
+        this.repo=repo;
+    }
+    
+    
+    public MultiChoice getMultiChoice(MultiChoice game){
+        return repo.getMultiChoice();
+    }
+  
 }
