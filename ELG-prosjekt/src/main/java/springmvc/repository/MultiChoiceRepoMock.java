86d01c47d6580d53b74cb05922945d4d5769c874
duckluck <tias.mahle@gmail.com>
2015-01-07 14:47:03 +0100

    Added extra mock exercises

--- ELG-prosjekt/src/main/java/springmvc/repository/MultiChoiceRepoMock.java
+++ ELG-prosjekt/src/main/java/springmvc/repository/MultiChoiceRepoMock.java
@@ -18,12 +18,21 @@ import springmvc.domain.MultiChoice;
 public class MultiChoiceRepoMock implements MultiChoiceRepository {
     private String[] alt = {"Alt 1", "Alt 2", "Alt 3", "Alt 4"};
     private Exercise e1 = new Exercise(alt, "Alt 2", "Hvem er riktig?");
+    private String[] alt2 = {"Alt 11", "Alt 22", "Alt 33", "Alt 44"};
+    private Exercise e2 = new Exercise(alt2, "Alt 2", "Dette er Ex2");
+    private String[] alt3 = {"Alt 111", "Alt 222", "Alt 333", "Alt 444"};
+    private Exercise e3 = new Exercise(alt3, "Alt 2", "Dette er Ex3");
+    private String[] alt4 = {"Alt 1", "Alt 2222", "Alt 3333", "Alt 4444"};
+    private Exercise e4 = new Exercise(alt4, "Alt 2", "Dette er Ex4");
     private ArrayList<Exercise> e = new ArrayList<>();
     private MultiChoice game = new MultiChoice(e, "Spill 1");
     private ArrayList<MultiChoice> games = new ArrayList<>();
     
     public MultiChoiceRepoMock(){
         e.add(e1);
+        e.add(e2);
+        e.add(e3);
+        e.add(e4);
         games.add(game);
     }
     
