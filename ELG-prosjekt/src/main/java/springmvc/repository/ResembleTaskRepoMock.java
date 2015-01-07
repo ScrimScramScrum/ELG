c5580a2fca623f1b0e0e53f911b4bb32ad67c985
borgarlie <borgar.lie@gmail.com>
2015-01-07 12:57:20 +0100

    created rest of the mock objects for resemble task

--- ELG-prosjekt/src/main/java/springmvc/repository/ResembleTaskRepoMock.java
+++ ELG-prosjekt/src/main/java/springmvc/repository/ResembleTaskRepoMock.java
@@ -3,15 +3,47 @@ package springmvc.repository;
 import springmvc.domain.ResembleTask;
 
 public class ResembleTaskRepoMock implements ResembleTaskRepo{
-    private String mockTaskText = "Du skal sette bakgrunnsfargen på siden til fargen #b0c4de<br>Du skal også lage en tabell med rød farge og to kolonner der det står 'hei' (border='1')"
-    private String mockSolutionHTML = "<!DOCTYPE html><html><body>Hei!<br><table border='1'><tr><td>hei</td><td>hei</td></tr></table></body></html>"; 
-    private String mockSolutionCSS = "body {background-color: #b0c4de;} table {background-color: red}";  
-    private String mockStartingHTML = "<!DOCTYPE html><html><body>Hei!</body></html>";  
-    private String mockStartingCSS = "body {}"; 
-    private int mockWidth = 100; 
-    private int mockHeight = 80; 
+    private String[] mockTaskText = new String[3];
+    private String[] mockSolutionHTML = new String[3];
+    private String[] mockSolutionCSS = new String[3]; 
+    private String[] mockStartingHTML = new String[3]; 
+    private String[] mockStartingCSS = new String[3]; 
+    private int mockWidth[] = new int[3];
+    private int mockHeight[] = new int[3];
+    
+    public ResembleTaskRepoMock() {
+        mockTaskText[0] = "Du skal sette bakgrunnsfargen på siden til fargen #b0c4de<br>Du skal også lage en tabell med rød farge og to kolonner der det står 'hei' (border='1')";
+        mockTaskText[1] = "Her skal vi teste noe nr 1";
+        mockTaskText[2] = "Her skal vi teste noe nr 2";
+
+        mockSolutionHTML[0] = "<!DOCTYPE html><html><body>Hei!<br><table border='1'><tr><td>hei</td><td>hei</td></tr></table></body></html>";
+        mockSolutionHTML[1] = "<html><body>supsup</body></html>";
+        mockSolutionHTML[2] = "<html><body>TEST NR 1<br>TEST NR 2<br>TEST NR 3</body></html>";
+        
+        mockSolutionCSS[0] = "body {background-color: #b0c4de;} table {background-color: red}";
+        mockSolutionCSS[1] = "";
+        mockSolutionCSS[2] = "";
+        
+        mockStartingHTML[0] = "<!DOCTYPE html><html><body>Hei!</body></html>";
+        mockStartingHTML[1] = "<html></html>";
+        mockStartingHTML[2] = "";
+        
+        mockStartingCSS[0] = "body {}";
+        mockStartingCSS[1] = "";
+        mockStartingCSS[2] = "";
+        
+        mockWidth[0] = 100;
+        mockWidth[1] = 200;
+        mockWidth[2] = 150;
+        
+        mockHeight[0] = 80;
+        mockHeight[1] = 150;
+        mockHeight[2] = 120;
+    }
     
     public ResembleTask getResembleTask(int taskNumber){
-        return new ResembleTask( taskNumber,  mockTaskText, mockSolutionHTML, mockSolutionCSS, mockStartingHTML, mockStartingCSS, mockWidth, mockHeight); 
+        if(taskNumber == 1) return new ResembleTask( taskNumber,  mockTaskText[0], mockSolutionHTML[0], mockSolutionCSS[0], mockStartingHTML[0], mockStartingCSS[0], mockWidth[0], mockHeight[0]); 
+        else if(taskNumber == 2) return new ResembleTask( taskNumber,  mockTaskText[1], mockSolutionHTML[1], mockSolutionCSS[1], mockStartingHTML[1], mockStartingCSS[1], mockWidth[1], mockHeight[1]);
+        else return new ResembleTask( taskNumber,  mockTaskText[2], mockSolutionHTML[2], mockSolutionCSS[2], mockStartingHTML[2], mockStartingCSS[2], mockWidth[2], mockHeight[2]);
     }
 }
