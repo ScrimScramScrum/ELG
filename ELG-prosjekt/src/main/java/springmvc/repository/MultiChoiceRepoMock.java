52079e119767073906e32fd80247c4c55653c369
duckluck <tias.mahle@gmail.com>
2015-01-08 10:28:54 +0100

    Fixed multichoice mock again

--- ELG-prosjekt/src/main/java/springmvc/repository/MultiChoiceRepoMock.java
+++ ELG-prosjekt/src/main/java/springmvc/repository/MultiChoiceRepoMock.java
@@ -19,22 +19,19 @@ public class MultiChoiceRepoMock implements MultiChoiceRepository {
     
     //Spill 1
     private String[] alt = {"Alt 1", "Alt 2", "Alt 3", "Alt 4"};
-    private Exercise e1 = new Exercise(alt, "Alt 2", "Hvem er riktig?");
-    private String[] alt2 = {"Alt 11", "Alt 22", "Alt 33", "Alt 44"};
-    private Exercise e2 = new Exercise(alt2, "Alt 11", "Dette er Ex2");
-    private String[] alt3 = {"Alt 111", "Alt 222", "Alt 333", "Alt 444"};
-    private Exercise e3 = new Exercise(alt3, "Alt 333", "Dette er Ex3");
-    private String[] alt4 = {"Alt 1", "Alt 2222", "Alt 3333", "Alt 4444"};
-    private Exercise e4 = new Exercise(alt4, "Alt 2", "Dette er Ex4");
+    private Exercise e1 = new Exercise(alt, "Alt 1", "Oppgavetekst her (Alt 1 er riktig)");
+    private Exercise e2 = new Exercise(alt, "Alt 2", "Oppgavetekst her (Alt 2 er riktig)");
+    private Exercise e3 = new Exercise(alt, "Alt 3", "Oppgavetekst her (Alt 3 er riktig)");
+    private Exercise e4 = new Exercise(alt, "Alt 4", "Oppgavetekst her (Alt 4 er riktig)");
     private ArrayList<Exercise> e = new ArrayList<>();
     private MultiChoice game = new MultiChoice(e, "Spill 1");
     private ArrayList<MultiChoice> games = new ArrayList<>();
     
     //Spill 2
-    private String[] alternatives = {"hei", "Hva skjer?", "Fett!", "Yeah"};
-    private Exercise exer1 = new Exercise(alternatives, "Hva skjer?", "Hvem er riktig?");
+    private String[] alternatives = {"Hei", "Hva skjer?", "Fett!", "Yeah"};
+    private Exercise exer1 = new Exercise(alternatives, "Hva skjer?", "Hvem er riktig? ('Hva skjer?' er riktig)");
     private String[] alternatives2 = {"Hola", "Que pasa?", "Cerveza!", "Ay Caramba!"};
-    private Exercise exer2 = new Exercise(alternatives2, "Cerveza!", "Mitt spanske favoritt ord er?");
+    private Exercise exer2 = new Exercise(alternatives2, "Cerveza!", "Mitt spanske favoritt ord er? ('Cerveza' er riktig)");
     private ArrayList<Exercise> exercise2 = new ArrayList<>();
     private MultiChoice game2 = new MultiChoice(exercise2, "Spill 2");
     
