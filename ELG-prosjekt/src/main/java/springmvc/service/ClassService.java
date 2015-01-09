1ab1dcc7ee05022e30b49bda83d5c91d68224e26
Bjørn Hoxmark <bhoxmark@gmail.com>
2015-01-09 13:39:39 +0100

    ClassService now has correct info.

--- ELG-prosjekt/src/main/java/springmvc/service/ClassService.java
+++ ELG-prosjekt/src/main/java/springmvc/service/ClassService.java
@@ -1,7 +1,30 @@
 package springmvc.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import springmvc.repository.ClassRepoDB;
+import springmvc.ui.AddNewClassId;
+
 
 public class ClassService {
+    
+    @Autowired
+    private ClassRepoDB classRepoDB;  
+    
+    public AddNewClassId getClassId(String classId){
+        return classRepoDB.getClassId(classId);        
+    }
+    
+    public boolean registrateNewClassId(AddNewClassId addNewClassId){
+                
+        if(classRepoDB.registerNewClassId(addNewClassId)){
+            System.out.println("Registered new Class in DB OK");            
+            return true;
+        } else {
+            System.out.println("Error in register class in DB");
+            return false;
+        }
+           
+    }
     
     
     
