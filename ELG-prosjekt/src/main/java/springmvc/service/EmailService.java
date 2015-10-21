a1ff5e249cdd61b442859423cda42413e5b591f8
Jørgen Wilhelmsen <jorgenwilhelmsen1@gmail.com>
2015-10-21 14:37:26 +0200

    Fixed POM issues

--- ELG-prosjekt/src/main/java/springmvc/service/EmailService.java
+++ ELG-prosjekt/src/main/java/springmvc/service/EmailService.java
@@ -1,36 +1,31 @@
 
 package springmvc.service;
 
-import java.util.Properties;
+import java.util.Properties; 
 import javax.mail.*;
 import javax.mail.internet.*;
 
-
-
-
 public class EmailService {
-    
-    
     public void sendEmail(String to, String fname, String lname, String password){
-    Session mailSession = createSmtpSession();
-    mailSession.setDebug (true);
-    String textMessage = "Hei "+fname.substring(0, 1).toUpperCase() + fname.substring(1).toLowerCase() +"! Her er ditt nye passord: "+password;
-    try {
-        Transport transport = mailSession.getTransport ();
+        Session mailSession = createSmtpSession();
+        mailSession.setDebug (true);
+        String textMessage = "Hei "+fname.substring(0, 1).toUpperCase() + fname.substring(1).toLowerCase() +"! Her er ditt nye passord: "+password;
+        try {
+            Transport transport = mailSession.getTransport ();
 
-        MimeMessage message = new MimeMessage (mailSession);
+            MimeMessage message = new MimeMessage (mailSession);
 
-        message.setSubject ("Ditt nye passord til ELG ved HiST");
-        message.setFrom (new InternetAddress ("ELGsystem.hist@gmail.com"));
-        message.setContent (textMessage, "text/html");
-        message.addRecipient (Message.RecipientType.TO, new InternetAddress (to));
+            message.setSubject ("Ditt nye passord til ELG ved HiST");
+            message.setFrom (new InternetAddress ("ELGsystem.hist@gmail.com"));
+            message.setContent (textMessage, "text/html");
+            message.addRecipient (Message.RecipientType.TO, new InternetAddress (to));
 
-        transport.connect ();
-        transport.sendMessage (message, message.getRecipients (Message.RecipientType.TO));  
-    }
-    catch (MessagingException e) {
-        e.printStackTrace();
-    }
+            transport.connect ();
+            transport.sendMessage (message, message.getRecipients (Message.RecipientType.TO));  
+        }
+        catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
     private Session createSmtpSession() {
@@ -48,7 +43,4 @@ public class EmailService {
                 }
         });
     }
-
 }
-    
-
